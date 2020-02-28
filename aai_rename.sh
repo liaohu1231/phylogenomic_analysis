652c63507a9bacd8876c0f7e830ec121f68a4c3b
liaohu1231 <59506038+liaohu1231@users.noreply.github.com>
2020-02-29 01:51:16 +0800

    Update aai_rename.sh

--- aai_rename.sh
+++ aai_rename.sh
@@ -1,1 +1,1 @@
-for i in $(less name.txt|cut -f 1);do j=$(less name.txt|sed -n "/$i/p"|cut -f 2);new=$i_$j;echo $i;sed -i 's/"$i"/"$new"/g' aai_colum3.txt;done
+for i in $(less name.txt|cut -f 1);do j=$(less name.txt|sed -n "/$i/p"|cut -f 2);new=$i"_"$j;echo $i;sed -i 's/"$i"/"$new"/g' aai_colum3.txt;done
